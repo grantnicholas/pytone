78699b4bed96af2c31fdb156d4a722bfb4f03a16
grantnicholas <grantnicholas2015@u.northwestern.edu>
2014-07-14 23:40:25 -0500

    Late-night changes to the signal processing. Drop dat bass tho

--- pytone.py
+++ pytone.py
@@ -3,9 +3,28 @@ from scipy.io import wavfile # get the api
 from scipy.fftpack import fft
 from pylab import *
 
+def max(arr):
+	maxval = arr[0];
+	for i, val in enumerate(arr):
+		if(val>maxval):
+			maxval = val;
+			index  = i;
+			
+	return (i,val);	
+
 
 def f(filename):
 	fs, data = wavfile.read(filename) # load the data
+	plt.plot(data.T[0]);
+	maxtimedomain = max(data.T[0])[1];
+	savefig('new'+filename+'.png',bbox_inches='tight');
+	
+	time_arr = [];
+	for i, val in enumerate(data.T[0]):
+		if(val>.5*maxtimedomain and data.T[0][i-100]>.5*maxtimedomain and data.T[0][i+100]>.5*maxtimedomain):
+			time_arr.append(i);
+	
+	print time_arr;
 	a = data.T[0] # this is a two channel soundtrack, I get the first track
 	b=[(ele/2**16.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
 	c = fft(b); # create a list of complex number
@@ -19,12 +38,10 @@ def f(filename):
 	plt.plot(frq,out,'r')
 	plt.ylim((0,50000))
 	savefig(filename+'.png',bbox_inches='tight')
-	max = 0;
-	for i in range(len(frq)):
-		if(out[i] > max):
-			max = frq[i];
+	maxi = max(frq)[1];
 			
-	print max;	
+	print maxi;
+
 		
 	
 	
